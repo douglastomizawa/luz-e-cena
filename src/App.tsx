8c738efbb7bc68a192a10cce9c50a6a14029eb3a
Douglas Tomizawa <tomizawadouglas@gmail.com>
2025-11-11 21:04:41 -0300

    feat: cria button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import Banner from "./components/banner";
+import Button from "./components/button";
 import Link from "./components/link/link";
 
 function App() {
   return (
     <>
-    <Banner src="./public/assets/imagens/bannerDesktop.png" alt="Luz e Cena" />
-    <Link href="/" target="_blank">Acesse nosso site</Link>
+      <Banner
+        src="./public/assets/imagens/bannerDesktop.png"
+        alt="Luz e Cena"
+      />
+      <Link href="/" target="_blank">
+        Acesse nosso site
+      </Link>
+      <Button variant="default">Click me</Button>
     </>
   );
 }
